e9a82a9a0364193f95fc44cc57860b893e4b73ea
Archana Kalathiya <archanakalathiya91@gmail.com>
2022-03-25 20:24:34 +0530

    pull

---
+++ C2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+
+    printf("Enter a and b : \n");
+    scanf("%d%d",&a,&b);
+
+    sum = a + b;
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
