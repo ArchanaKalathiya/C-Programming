431f50ceebd84ba69ac901794fe69793eca9aa19
Archana Kalathiya <archanakalathiya91@gmail.com>
2022-03-25 21:01:00 +0530

    commit

---
+++ C3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("\nint : %lu \n", (sizeof(int)));
+    printf("\nfloat : %lu \n", (sizeof(float)));
+    printf("\nchar : %lu \n", (sizeof(char)));
+    printf("\nlong : %lu \n", (sizeof(long)));
+    printf("\ndouble : %lu \n", (sizeof(double)));
+    return 0;
+}
