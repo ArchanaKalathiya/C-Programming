d017752d90a0325736091ee325de5feef0956035
Archana Kalathiya <archanakalathiya91@gmail.com>
2022-04-30 14:36:02 +0530

    Functions

---
+++ C11.c
@@ -0,0 +1,13 @@
+// Functioons
+// Program that defines and calls a simple function
+#include <stdio.h>
+void main()
+{
+    printf("\nHello Friends");
+    message();
+    printf("\nFunctions in C");
+}
+message()
+{
+    printf("\nLet us Learn");
+}
