302fd6709a83d481356a6d374c4d6eab19d9478e
Archana Kalathiya <archanakalathiya91@gmail.com>
2022-07-21 19:57:18 +0530

    Nested loop

---
+++ C++/C6.cpp
@@ -0,0 +1,19 @@
+//Nested loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int j;
+    for ( i = 0; i <= 10 ; i++)
+    {
+        cout<<"\n";
+        for ( j = 1; j <= i; j++)
+        {
+            
+            cout<<"*";
+        }
+        
+    }
+    
+}
