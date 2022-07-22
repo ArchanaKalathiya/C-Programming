d25b2c3f2de08bedabc6cb2a75cdc88d9e006cd2
Archana Kalathiya <archanakalathiya91@gmail.com>
2022-07-22 16:04:17 +0530

    Function

---
+++ C++/Excercise/Ex-2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void Increment(int &s)
+{
+    //passing address of the variable
+    s=s+5000;
+    cout<<"Salary value inside increment function: "<<s<<endl;
+}
+int main()
+{
+    int sal=27000;
+    Increment(sal);
+    cout<<"Salary value inside main function : "<<sal<<endl;//
+    return 0;
+}
