f21cb2c6b3804ecf9346dcf0c3a1339a6cc1b82f
Archana Kalathiya <archanakalathiya91@gmail.com>
2022-07-23 15:52:13 +0530

    Loops

---
+++ C-Programming/C++/Excercise/Loops/Ex-1.cpp
@@ -0,0 +1,19 @@
+//Write a program of printing a pattern in square using loops
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size;
+    int chr;
+    cout<<"Enter sqaure size: ";
+    cin>>size;
+    for ( int i = 1; i <= size ; i++)
+    {
+        for ( int j = 1; j <= size; j++)
+        {
+            cout<<"  * ";
+        } 
+        cout<<endl;
+    }
+    return 0;
+}
