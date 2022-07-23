cb2596df9c89685ba3180a8c02e51dccb82e2b43
Archana Kalathiya <94705812+ArchanaKalathiya@users.noreply.github.com>
2022-07-24 04:45:56 +0530

    Loops

---
+++ C++/Excercise/Loops/Ex-4.cpp
@@ -0,0 +1,14 @@
+// Print multiplication table of 5
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num, prod;
+    cout << "Enter a number of table you want : ";
+    cin >> num;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << num << " x " << i << " = " << num * i << endl;
+    }
+    return 0;
+}
