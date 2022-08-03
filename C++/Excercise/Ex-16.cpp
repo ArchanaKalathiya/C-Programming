0b19fe9597b918deaaa47899fa8cf347d939cddb
Archana Kalathiya <94705812+ArchanaKalathiya@users.noreply.github.com>
2022-08-03 20:43:36 +0530

    Function without return

---
+++ C++/Excercise/Ex-16.cpp
@@ -0,0 +1,11 @@
+//Without return 
+#include<iostream>
+using namespace std;
+void Num(int num1){
+    cout<<"The number is : "<<num1<<endl;
+}
+int main(){
+    int number=60;
+    Num(number);
+    return 0;
+}
