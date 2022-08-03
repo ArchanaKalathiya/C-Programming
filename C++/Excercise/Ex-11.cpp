dddf3b8d01661b75b4da3f57ac2f2c4fad540de8
Archana Kalathiya <94705812+ArchanaKalathiya@users.noreply.github.com>
2022-08-03 18:48:47 +0530

    Call by Reference

---
+++ C++/Excercise/Ex-11.cpp
@@ -0,0 +1,13 @@
+//Call by Refernce 
+#include<iostream>
+using namespace std;
+void increment(int &a){
+    a++;
+    cout<<"Value in Function increment : "<<a<<endl;
+}
+int main(){
+    int x=5;
+    increment(x);
+    cout<<"Value in Function main : "<<x<<endl;
+    return 0;
+}
