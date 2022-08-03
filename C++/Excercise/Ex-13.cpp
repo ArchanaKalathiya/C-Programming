ded19f512e70448e1863f1c83b596c3f38ed5d23
Archana Kalathiya <94705812+ArchanaKalathiya@users.noreply.github.com>
2022-08-03 20:41:54 +0530

    Function with parameters

---
+++ C++/Excercise/Ex-13.cpp
@@ -0,0 +1,13 @@
+//With Parameters
+#include<iostream>
+using namespace std;
+void displayNum(int n1,float n2){
+    cout<<"The int number is : "<<n1<<endl;
+    cout<<"The double number is : "<<n2<<endl;
+}
+int main(){
+    int num1=5;
+    double num2=25.33;
+    displayNum(num1,num2);
+    return 0;
+}
